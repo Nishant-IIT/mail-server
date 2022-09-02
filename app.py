16f1f551d660a2eb06b792019356bcd2b7485900
Nishant Bhagat <nishantbhagat435@gmail.com>
2022-09-02 11:49:29 +0530

    Initial commit

---
+++ app.py
@@ -0,0 +1,35 @@
+from flask import Flask
+from flask import request
+from flask import json
+from flask import jsonify
+from flask_cors import CORS, cross_origin
+import os
+from sendgrid import SendGridAPIClient
+from sendgrid.helpers.mail import Mail
+
+app = Flask(__name__)
+cors = CORS(app)
+app.config["CORS_HEADERS"] = "Content-Type"
+@BB.route("/getBBands", methods=["POST"])
+@cross_origin()
+def sendMail():
+    to_email = request.json["to_email"]
+    if request.method == "POST":
+
+        message = Mail(
+        from_email='nishantbhagat435@gmail.com',
+        to_emails=to_email,
+        subject='You have successfully subscribed to our Newsletter!',
+        html_content='<strong>You have successfully subscribed to our Newsletter!</strong>')
+        try:
+            sg = SendGridAPIClient(os.environ.get('SENDGRID_API_KEY'))
+            response = sg.send(message)
+            print(response.status_code)
+            print(response.body)
+            print(response.headers)
+        except Exception as e:
+            print(e)
+
+
+if __name__ == "__main__":
+    app.run()
